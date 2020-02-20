a346ed16a4d05ca46f9248bd7cf849c67f471cfd
l-k-mist <dylan@vandenbosch.co.za>
2020-02-20 15:25:12 +0200

    externall scss

--- README.md
+++ README.md
@@ -43,6 +43,9 @@ These two serve up the required js and css
 https://cdn.jsdelivr.net/gh/L-K-Mist/pan-slider-vue-source@latest/dist/js/app.js
 https://cdn.jsdelivr.net/gh/L-K-Mist/pan-slider-vue-source@latest/dist/css/app.css
 
+When I've made important changes, I must run this link to speed up the renewal for consumers of the code.
+https://purge.jsdelivr.net/gh/L-K-Mist/pan-slider-vue-source@latest/dist/js/app.js
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/L-K-Mist/pan-slider-vue-source@latest/dist/js/app.js"></script>
 
     		```
--- src/components/KenBurns.vue
+++ src/components/KenBurns.vue
@@ -36,42 +36,4 @@ export default {
 };
 </script>
 
-<style>
-.imageContainer {
-  background-color: #333;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  transform: scaleY(0.9);
-  /* border: 2px #333 solid; */
-}
-.imageContainer img {
-  /* transform: translateY(-1%); */
-}
-@keyframes kenburns {
-  0% {
-    opacity: 0.6;
-  }
-  35% {
-    opacity: 1;
-  }
-  /* 75% {
-    transform: scale3d(1.2, 1.2, 1.2);
-    animation-timing-function: ease-in;
-    opacity: 1;
-  } */
-  100% {
-    transform: scale3d(1.4, 1.4, 1.4) translate3d(10%, 6%, 0);
-    animation-timing-function: ease-in;
-    opacity: 1;
-  }
-}
-
-/* #imageContainer img.animate {
-  animation: kenburns 20s infinite;
-} */
-
-/* #theImage.animation {
-  animation: kenburns 5s forwards;
-} */
-</style>
+<style lang="scss"></style>
